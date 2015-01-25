a6c3ec30808b354a4d121556c73f3a158dffd23d
Ursula Sarracini <ursulasarracini@gmail.com>
2015-01-25 17:58:29 -0500

    added map functionalities

---
+++ js/map.js
@@ -0,0 +1,16 @@
+function initialize() {
+            var mapCanvas = document.getElementById('map');
+            var mapOptions = {
+                center: new google.maps.LatLng(43.8452, -79.6280),
+                zoom: 15,
+                scrollwheel: false,
+                navigationControl: false,
+                mapTypeControl: false,
+                scaleControl: false,
+                disableDoubleClickZoom: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+                
+            }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+          google.maps.event.addDomListener(window, 'load', initialize);
